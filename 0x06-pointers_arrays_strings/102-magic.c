c625d4f372f785c6b45490ffffb9b73fd6604afd
Wasiu Akindoyin <96507307+Wasiu-Akindoyin@users.noreply.github.com>
2022-09-23 17:49:57 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Description: A program that prints a[2] = 98, followed by a new line.
- * Return: Always 0 (Success)
- */
 
 int main(void)
 {
@@ -15,6 +10,11 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
